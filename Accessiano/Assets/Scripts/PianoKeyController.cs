a68393a1b7813d6d450a964a7e88c25bc9bd63b3
Yuval Gabay <yuval.gabay@outlook.com>
2019-04-30 20:35:34 +0300

    changed to 3D Piano

---
+++ Accessiano/Assets/Scripts/PianoKeyController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoKeyController : MonoBehaviour
+{
+
+    private bool isTrigger = false;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        //play sound
+        if (Input.GetKeyDown("space") && (isTrigger))
+        {
+            OnPlay();
+        }
+    }
+
+    void OnPlay()
+    {
+        Debug.Log("enter to OnPlay()!!!!!");
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Eye")
+        {
+            this.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Eye")
+        {
+            this.isTrigger = false;
+        }
+    }
+}
